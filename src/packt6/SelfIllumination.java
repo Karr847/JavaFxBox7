e6b161e200e14506ff5d5199161982f17f19730e
Ka847 <mohammed_arr@hotmail.com>
2021-09-28 14:15:19 +0100

    Initial commit

---
+++ src/packt6/SelfIllumination.java
@@ -0,0 +1,154 @@
+package packt6;
+
+
+
+import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.*;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Material;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Transform;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+public class SelfIllumination extends Application {
+
+    private static final double V = 800;
+    private static final double V_1 = 600;
+
+    private double anchorX,anchorY;
+    private double anchorAngleX=0;
+    private double anchorAngleY=0;
+    private final DoubleProperty angleX=new SimpleDoubleProperty ( 0 );
+    private final DoubleProperty angleY=new SimpleDoubleProperty ( 0 );
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Box box = prepareBox ( );
+        SmartGroup group = new SmartGroup ( );
+        group.getChildren ( ).add (box);
+        group.getChildren ( ).addAll (prepareLightSource ( ));
+
+        //1.Ambient light
+        //2.point light
+        Camera camera = new PerspectiveCamera ( );
+        Scene scene = new Scene (group, V, V_1);
+        scene.setFill (Color.SILVER);
+        scene.setCamera (camera);
+        group.translateXProperty ( ).set (V / 2);
+        group.translateYProperty ( ).set (V_1 / 2);
+        group.translateZProperty ( ).set (-850);
+        initMouseControl (group, scene, primaryStage);
+
+        primaryStage.addEventHandler (KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode ( )) {
+                case W:
+
+                    group.translateZProperty ( ).set (group.getTranslateZ ( ) + 100);
+                    break;
+                case S:
+                    group.translateZProperty ( ).set (group.getTranslateZ ( ) - 100);
+                    break;
+                case Q:
+                    group.rotateByX (10);
+                    break;
+                case E:
+                    group.rotateByX (-10);
+                    break;
+                case NUMPAD6:
+                    group.rotateByY (10);
+                    break;
+                case NUMPAD4:
+
+                    group.rotateByY (-10);
+                    break;
+
+
+            }
+        });
+
+
+        primaryStage.setTitle ("Cuboid Transformation 3D ");
+        primaryStage.setScene (scene);
+        primaryStage.show ( );
+    }
+    private Node[] prepareLightSource(){
+    //    AmbientLight ambientLight=new AmbientLight ();
+     //   ambientLight.setColor (Color.AQUA);
+     //   return ambientLight;
+        PointLight pointLight=new PointLight ();
+        pointLight.setColor (Color.RED);
+        pointLight.getTransforms ().add (new Translate (0,-5,100));
+        Sphere sphere=new Sphere (2);
+        sphere.getTransforms ().setAll (pointLight.getTransforms ());
+        return new Node[]{pointLight,sphere};
+
+    }
+    private Box prepareBox(){
+        PhongMaterial material=new PhongMaterial ();
+        Box box=new Box ( 100,20,50 );
+        material.setDiffuseMap (new Image (getClass ().getResourceAsStream ("/resources/wooden texture.jpg")));
+        material.setSpecularColor (Color.WHITE);
+        box.setMaterial (material);
+        return box;
+    }
+    private void initMouseControl(SmartGroup group, Scene scene,Stage stage){
+        Rotate xRotate;
+        Rotate yRotate;
+        group.getTransforms ().addAll (
+                xRotate=new Rotate ( 0,Rotate.X_AXIS ),
+                yRotate=new Rotate ( 0,Rotate.Y_AXIS )
+        );
+        xRotate.angleProperty ().bind (angleX);
+        yRotate.angleProperty ().bind (angleY);
+        scene.setOnMousePressed (event->{
+            anchorX=event.getSceneX ();
+            anchorY=event.getSceneY ();
+            anchorAngleX=angleX.get ();
+            anchorAngleY=angleY.get ();
+
+        });
+        scene.setOnMouseDragged (event->{
+            angleX.set (anchorAngleX-(anchorY-event.getSceneY ()));
+            angleY.set (anchorAngleY+anchorX-event.getSceneX ());
+
+        });
+        stage.addEventHandler (ScrollEvent.SCROLL, event->{
+            double delta=event.getDeltaY ();
+            group.translateZProperty ().set (group.getTranslateZ()+delta);
+        });
+
+    }
+
+    public static void main(String[] args) {
+        launch (args);}
+    class SmartGroup extends Group {
+        Rotate r;
+        Transform t = new Rotate ( );
+
+        void rotateByX(int ang) {
+            r = new Rotate (ang, Rotate.X_AXIS);
+            t = t.createConcatenation (r);
+            this.getTransforms ( ).clear ( );
+            this.getTransforms ( ).addAll (t);
+        }
+
+        void rotateByY(int ang) {
+            r = new Rotate (ang, Rotate.Y_AXIS);
+            t = t.createConcatenation (r);
+            this.getTransforms ( ).clear ( );
+            this.getTransforms ( ).addAll (t);
+        }
+    }
+}
+
+
